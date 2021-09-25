8466f07aec9b079b1953377d9b08429e370b7503
Andrey Bienkowski <hexagon-recursion@posteo.net>
2021-09-25 08:33:26 +0300

    Remove dead code
    Dead code is a code smell: https://refactoring.guru/es/smells/dead-code
    
    For now I'll only delete this two files. If this PR is merged, I'll
    open a new PR to remove more dead code
    
    Testing:
    1. The game compiles
    2. The game starts

--- src/main.cpp
+++ src/main.cpp
@@ -160,10 +160,6 @@
 #  include "ZOs_Specific_Various.h"
 #endif
 
-#ifndef Z_ZTEST_PARTS_H
-#  include "Z0Test_Parts.h"
-#endif
-
 
 ZGame * Ge;
 double FrameTime;
@@ -183,15 +179,6 @@ int main(int argc, char *argv[])
   // Start
 
     printf ("Starting BlackVoxel...\n");
-
-  // Test Code
-
-  #if DEVELOPPEMENT_ON == 1
-/*
-     ZTest_Parts TestParts;
-     if (!TestParts.RunTestCode()) exit(0);
-*/
-  #endif
 
   // Game main object
 
